45f947b7c77a37fd9f035ad3ce889352e9f10579
RomanDovgii <romandovgiiwork@gmail.com>
2021-02-02 00:44:26 +0300

    Almost final

--- source/js/menu-button.js
+++ source/js/menu-button.js
@@ -1,5 +1,7 @@
 const HEADER_OPEN = `header--open`;
+const HEADER_CLOSE = `header--close`;
 const BUTTON_CLOSE = `header__button--close`;
+const HEADER_BREAKPOINT = 1200;
 
 const button = document.querySelector(`.header__button`);
 const header = document.querySelector(`.header`);
@@ -7,13 +9,39 @@ const header = document.querySelector(`.header`);
 const handleMenu = () => {
   if (button.classList.contains(`header__button--close`)) {
     header.classList.remove(HEADER_OPEN);
+    header.classList.add(HEADER_CLOSE);
     button.classList.remove(BUTTON_CLOSE);
+    button.disabled = true;
+
+    setTimeout(() => {
+      header.classList.remove(HEADER_CLOSE);
+      button.disabled = false;
+    }, 1000);
+
     return;
   }
 
+  header.classList.remove(HEADER_CLOSE);
   header.classList.add(HEADER_OPEN);
   button.classList.add(BUTTON_CLOSE);
+  button.disabled = true;
+
+  setTimeout(() => {
+    button.disabled = false;
+  }, 600);
+
+  return;
+}
+
+const removeClasses = () => {
+  if (window.screen.width > HEADER_BREAKPOINT) {
+    header.classList.remove(HEADER_OPEN);
+    header.classList.remove(HEADER_CLOSE);
+    button.classList.remove(BUTTON_CLOSE);
+    return;
+  }
   return;
 }
 
 button.addEventListener(`click`, handleMenu);
+window.addEventListener('resize', removeClasses);
